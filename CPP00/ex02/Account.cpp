156f573536adb59b88bf91dd4cb5726fded21001
Ricky Wong <rickywong0625@gmail.com>
2023-07-17 14:19:21 +0800

    updated Account.cpp

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -12,6 +12,7 @@
 
 #include "Account.hpp"
 #include <iostream>
+#include <ctime>
 
 using std::string;
 
@@ -20,12 +21,14 @@ enum    e_obj_state {
     DESTRUCT
 };
 
+// Initialize static data members.
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
-static void log_obj_state(int index, int amount, e_obj_state state) {
+// Log account state to stdout based on the given state.
+static void _logAccountState(int index, int amount, e_obj_state state) {
 
     const char *state_str;    
     
@@ -35,40 +38,49 @@ static void log_obj_state(int index, int amount, e_obj_state state) {
     std::cout << "index:" << index << ";amount:" << amount << ";" << state_str << "\n";
 }
 
-Account::Account(void) {}
+// Default constructor. Note that this function does not declared in the header file.
+Account::Account() {}
 
+// Overloaded constructor.
+// Note that initialization lists were not used here because intialization lists
+// can only initialize non-static data members.
 Account::Account(int initial_deposit) {
-    this->_accountIndex = _nbAccounts++;
-    this->_amount = initial_deposit;
-    this->_nbDeposits = 0;
-    this->_nbWithdrawals = 0;
+    _accountIndex = _nbAccounts++;
+    _amount = initial_deposit;
     _totalAmount += initial_deposit;
     _displayTimestamp();
-    log_obj_state(this->_accountIndex, this->_amount, INSTANTIATE);
+    _logAccountState(_accountIndex, _amount, INSTANTIATE);
 }
 
-Account::~Account(void) {
+// Destructor.
+Account::~Account() {
     _displayTimestamp();
-    log_obj_state(this->_accountIndex, this->_amount, DESTRUCT);
+    _logAccountState(_accountIndex, _amount, DESTRUCT);
 }
 
-int     Account::getNbAccounts(void) {
+// A static member getter function.
+// when define a static member function, the static keyword is not required
+int     Account::getNbAccounts() {
     return _nbAccounts;
 }
 
-int     Account::getTotalAmount(void) {
+// A static member getter function.
+int     Account::getTotalAmount() {
     return _totalAmount;
 }
 
-int     Account::getNbDeposits(void) {
+// A static member getter function.
+int     Account::getNbDeposits() {
     return _totalNbDeposits;
 }
 
-int     Account::getNbWithdrawals(void) {
+// A static member getter function.
+int     Account::getNbWithdrawals() {
     return _totalNbWithdrawals;
 }
 
-void    Account::displayAccountsInfos(void) {
+// A static member function. Show the current state of the account.
+void    Account::displayAccountsInfos() {
     _displayTimestamp();
     std::cout << "accounts:" << getNbAccounts() << ";";
     std::cout << "total:" << getTotalAmount() << ";";
@@ -76,23 +88,25 @@ void    Account::displayAccountsInfos(void) {
     std::cout << "withdrawals:" << getNbWithdrawals() << "\n";
 }
 
+// A member function. Make a deposit.
 void    Account::makeDeposit(int deposit) {
     _totalAmount += deposit;
     ++_totalNbDeposits;
-    ++this->_nbDeposits;
+    ++_nbDeposits;
     _displayTimestamp();
-    std::cout << "index:" << this->_accountIndex << ";";
-    std::cout << "p_amount:" << this->_amount << ";";
+    std::cout << "index:" << _accountIndex << ";";
+    std::cout << "p_amount:" << _amount << ";";
     std::cout << "deposit:" << deposit << ";";
-    this->_amount += deposit;
-    std::cout << "amount:" << this->_amount << ";";
-    std::cout << "nb_deposits:" << this->_nbDeposits << "\n";
+    _amount += deposit;
+    std::cout << "amount:" << _amount << ";";
+    std::cout << "nb_deposits:" << _nbDeposits << "\n";
 }
 
+// A member function. Make a withdrawal.
 bool    Account::makeWithdrawal(int withdrawal) {
     _displayTimestamp();
-    std::cout << "index:" << this->_accountIndex << ";";
-    std::cout << "p_amount:" << this->_amount << ";";
+    std::cout << "index:" << _accountIndex << ";";
+    std::cout << "p_amount:" << _amount << ";";
     std::cout << "withdrawal:";
     if (checkAmount() < withdrawal || withdrawal < 0)
     {
@@ -101,27 +115,38 @@ bool    Account::makeWithdrawal(int withdrawal) {
     }
     std::cout << withdrawal << ";";
     _totalAmount -= withdrawal;
-    this->_amount -= withdrawal;
+    _amount -= withdrawal;
     ++_totalNbWithdrawals;
-    ++this->_nbWithdrawals;
-    std::cout << "amount:" << this->_amount << ";";
-    std::cout << "nb_withdrawals:" << this->_nbWithdrawals << "\n";
+    ++_nbWithdrawals;
+    std::cout << "amount:" << _amount << ";";
+    std::cout << "nb_withdrawals:" << _nbWithdrawals << "\n";
     return true;
 }
 
+// Getter. Return the current amount of the account.
 int     Account::checkAmount(void) const {
-    return (this->_amount);
+    return (_amount);
 }
 
+// A member function. Show the current state of the account.
 void    Account::displayStatus(void) const {
     _displayTimestamp();
-    std::cout << "index:" << this->_accountIndex << ";";
-    std::cout << "amount:" << this->_amount << ";";
-    std::cout << "deposits:" << this->_nbDeposits << ";";
-    std::cout << "withdrawals:" << this->_nbWithdrawals << "\n";
+    std::cout << "index:" << _accountIndex << ";";
+    std::cout << "amount:" << _amount << ";";
+    std::cout << "deposits:" << _nbDeposits << ";";
+    std::cout << "withdrawals:" << _nbWithdrawals << "\n";
 }
 
-void    Account::_displayTimestamp(void) {
+/**
+ * @brief Display current timestamp in the following format: [YYYYMMDD_HHMMSS]
+ * 
+ * @details
+ * 1. Get current time in seconds since epoch.
+ * 2. Convert current time to local time.
+ * 3. Format local time to string.
+ * 4. Print formatted time to stdout.
+*/
+void    Account::_displayTimestamp() {
     char        timestamp[16];
     std::time_t current_time = std::time(NULL);
 
