d9bd6b19d89430149036157dfe60ba88218ecb40
Ricky0625 <rickywong0625@gmail.com>
2023-07-31 21:29:00 +0800

    updated ex00

--- CPP07/ex00/srcs/main.cpp
+++ CPP07/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wricky-t <wricky-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/29 21:28:32 by wricky-t          #+#    #+#             */
-/*   Updated: 2023/07/31 18:55:12 by wricky-t         ###   ########.fr       */
+/*   Updated: 2023/07/31 19:10:36 by wricky-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int main(void)
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
     std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+    TEST_NL();
 
     std::string c = "chaine1";
     std::string d = "chaine2";
@@ -31,6 +32,7 @@ int main(void)
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
     std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+    TEST_NL();
     
     float e = 1.123f;
     float f = 1.122f;
@@ -41,6 +43,7 @@ int main(void)
     std::cout << "max( e, f ) = " << ::max(e, f) << std::endl;
     std::cout << "min( g, f ) = " << ::min(g, f) << std::endl;
     std::cout << "max( g, f ) = " << ::max(g, f) << std::endl;
+    TEST_NL();
 
     double h = 12431523423.2453463124124;
     double i = 1412523623142464.23613462241;
@@ -48,5 +51,21 @@ int main(void)
     std::cout << "h = " << h << ", i = " << i << std::endl;
     std::cout << "min( h, i ) = " << ::min(h, i) << std::endl;
     std::cout << "max( h, i ) = " << ::max(h, i) << std::endl;
-    return 0;
+    TEST_NL();
+
+    char j = 'J';
+    char k = 'K';
+    ::swap(j, k);
+    std::cout << "j = " << j << ", k = " << k << std::endl;
+    std::cout << "min( j, k ) = " << ::min(j, k) << std::endl;
+    std::cout << "max( j, k ) = " << ::max(j, k) << std::endl;
+    TEST_NL();
+
+    bool l = true;
+    bool m = false;
+    ::swap(l, m);
+    std::cout << "l = " << (l ? "true" : "false") << ", m = " << (m ? "true" : "false") << std::endl;
+    std::cout << "min( l, m ) = " << (::min(l, m) ? "true" : "false") << std::endl;
+    std::cout << "max( l, m ) = " << (::max(l, m) ? "true" : "false") << std::endl;
+    TEST_NL();
 }
